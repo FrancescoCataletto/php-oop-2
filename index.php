f0a112262b64da658885a265c8f955ff67aa971e
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-06-20 14:04:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$prova = "Funziona"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop-2</title>
+</head>
+<body>
+    <h1><?php echo $prova ?></h1>
+</body>
+</html>
